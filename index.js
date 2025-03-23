bcfb76b434f4385869ee5b583850d561a158b9c1
Asiya Ikhlaq <ikhlaqasiya7@gmail.com>
2025-03-23 00:39:45 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var email = prompt("enter your email");
+var passaword = prompt("enter your password")
+if(email===passaword){
+    document.write(
+        "login successfuly"
+    )
+}
+else{
+    document.write("invaild password  or email")
+}
+
